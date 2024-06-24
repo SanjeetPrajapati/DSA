d65ec62374e084f70a3695a5e57834d54fc6a058
Sanjeet Prajapati <prajapatisanjeet50@gmail.com>
2024-06-24 22:57:19 +0530

    testing

---
+++ tp.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    
+    vector<int> v;
+    cout<<"size is" << v.size()<<endl;
+   
+
+    cout<<v.capacity();
+   
+    return 0;
+}
